40b98fb48ee46f679f9fee3ca9089f37ceb0259c
Lorenzo Fontoura <lorenzo@nioel.com>
2021-02-28 01:56:47 +1000

    fix node installation script

--- node.sh
+++ node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash ~/startapp/git-init.sh
-apt-get -y curl
-curl -0 -L https://npmjs.org/install.sh | sh
+DEBIAN_FRONTEND=noninteractive apt-get -y install nodejs curl
+DEBIAN_FRONTEND=noninteractive curl -0 -L https://npmjs.org/install.sh | sh
 npm install --global n
 n $NODE_VERSION
 cd $PROJECT_DIRECTORY
