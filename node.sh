809ba805549119980f1e255742cd3ab0ad02baad
Lorenzo Fontoura <lorenzo@nioel.com>
2021-02-28 00:48:51 +1000

    update node installation

--- node.sh
+++ node.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 bash ~/startapp/git-init.sh
-apt-get -y install npm
+apt-get -y curl
+curl -0 -L https://npmjs.org/install.sh | sh
+npm install --global n
+n $NODE_VERSION
 cd $PROJECT_DIRECTORY
 npm install
 npm start
